6ff14f091a5fb74dc73432f443195f4380eda7f1
wuyu <191287278@qq.com>
2019-01-01 01:12:56 +0800

    init

--- src/main/java/com/vcg/mybatis/example/processor/MybatisExampleRepository.java
+++ src/main/java/com/vcg/mybatis/example/processor/MybatisExampleRepository.java
@@ -2,7 +2,6 @@ package com.vcg.mybatis.example.processor;
 
 import org.apache.ibatis.annotations.Param;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,38 +41,30 @@ public interface MybatisExampleRepository<T, ID, Example> {
 
     boolean existsById(ID id);
 
-    Iterable<T> findAll();
-
-    Iterable<T> findAllById(Iterable<ID> ids);
-
     long count();
 
     void deleteById(ID id);
 
     void delete(T entity);
 
-    void deleteAll(Iterable<? extends T> entities);
+    void deleteAll(List<? extends T> entities);
+
+    List<T> findAll();
+
+    List<T> findAllById(List<ID> ids);
 
     default Optional<T> findById(ID id) {
         T t = selectByPrimaryKey(id);
         return t != null ? Optional.of(t) : Optional.empty();
     }
 
-    default <S extends T> S save(S entity) {
+    default T save(T entity) {
         insertSelective(entity);
         return entity;
     }
 
-    default <S extends T> Iterable<S> saveAll(Iterable<S> entities) {
-        if (entities instanceof List) {
-            insertBatch((List<T>) entities);
-        } else {
-            List<T> list = new ArrayList<>();
-            for (S entity : entities) {
-                list.add(entity);
-            }
-            insertBatch(list);
-        }
+    default List<T> saveAll(List<T> entities) {
+        insertBatch(entities);
         return entities;
     }
 }
