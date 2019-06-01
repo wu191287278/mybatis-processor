fe867f6442ff228a1abe63c3c707f29a9c8b54ce
wuyu <191287278@qq.com>
2019-06-01 22:46:52 +0800

    优化exists

--- mybatis-processor-starter/src/main/java/com/vcg/mybatis/example/starter/MybatisQueryCreator.java
+++ mybatis-processor-starter/src/main/java/com/vcg/mybatis/example/starter/MybatisQueryCreator.java
@@ -33,7 +33,7 @@ public class MybatisQueryCreator extends AbstractQueryCreator<String, StringBuil
 
     private static final String COUNT_SQL = "select count(*) from <include refid=\"JqlTableName\" /> ";
 
-    private static final String EXIST_SQl = "select 1 from  <include refid=\"JqlTableName\" /> ";
+    private static final String EXIST_SQl = "select 1 from  <include refid=\"JqlTableName\" /> limit 1";
 
     private static final String DELETE_SQl = "delete from <include refid=\"JqlTableName\" />  ";
 
