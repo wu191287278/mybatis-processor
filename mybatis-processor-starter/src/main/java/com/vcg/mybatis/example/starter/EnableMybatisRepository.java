225f7ea9971af12dd163a67a1593f52c998452d7
wuyu <191287278@qq.com>
2024-04-03 15:02:35 +0800

    支持聚合函数

--- mybatis-processor-starter/src/main/java/com/vcg/mybatis/example/starter/EnableMybatisRepository.java
+++ mybatis-processor-starter/src/main/java/com/vcg/mybatis/example/starter/EnableMybatisRepository.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScannerRegistrar;
 import org.mybatis.spring.mapper.MapperFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanNameGenerator;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.annotation.AliasFor;
@@ -75,64 +76,100 @@ public @interface EnableMybatisRepository {
     String[] basePackages() default {};
 
     /**
-     * Type-safe alternative to {@link #basePackages()} for specifying the packages
-     * to scan for annotated components. The package of each class specified will be scanned.
-     * <p>Consider creating a special no-op marker class or interface in each package
-     * that serves no purpose other than being referenced by this attribute.
-     * @return Class.
+     * Type-safe alternative to {@link #basePackages()} for specifying the packages to scan for annotated components. The
+     * package of each class specified will be scanned.
+     * <p>
+     * Consider creating a special no-op marker class or interface in each package that serves no purpose other than being
+     * referenced by this attribute.
+     *
+     * @return classes that indicate base package for scanning mapper interface
      */
     Class<?>[] basePackageClasses() default {};
 
     /**
-     * The {@link BeanNameGenerator} class to be used for naming detected components
-     * within the Spring container.
-     * @return Class.
+     * The {@link BeanNameGenerator} class to be used for naming detected components within the Spring container.
+     *
+     * @return the class of {@link BeanNameGenerator}
      */
     Class<? extends BeanNameGenerator> nameGenerator() default BeanNameGenerator.class;
 
     /**
      * This property specifies the annotation that the scanner will search for.
      * <p>
-     * The scanner will register all interfaces in the base package that also have
-     * the specified annotation.
+     * The scanner will register all interfaces in the base package that also have the specified annotation.
      * <p>
      * Note this can be combined with markerInterface.
-     * @return Class
+     *
+     * @return the annotation that the scanner will search for
      */
     Class<? extends Annotation> annotationClass() default Annotation.class;
 
     /**
      * This property specifies the parent that the scanner will search for.
      * <p>
-     * The scanner will register all interfaces in the base package that also have
-     * the specified interface class as a parent.
+     * The scanner will register all interfaces in the base package that also have the specified interface class as a
+     * parent.
      * <p>
      * Note this can be combined with annotationClass.
-     * @return Class.
+     *
+     * @return the parent that the scanner will search for
      */
     Class<?> markerInterface() default Class.class;
 
     /**
-     * Specifies which {@code SqlSessionTemplate} to use in the case that there is
-     * more than one in the spring context. Usually this is only needed when you
-     * have more than one datasource.
-     * @return String.
+     * Specifies which {@code SqlSessionTemplate} to use in the case that there is more than one in the spring context.
+     * Usually this is only needed when you have more than one datasource.
+     *
+     * @return the bean name of {@code SqlSessionTemplate}
      */
     String sqlSessionTemplateRef() default "";
 
     /**
-     * Specifies which {@code SqlSessionFactory} to use in the case that there is
-     * more than one in the spring context. Usually this is only needed when you
-     * have more than one datasource.
-     * @return String.
+     * Specifies which {@code SqlSessionFactory} to use in the case that there is more than one in the spring context.
+     * Usually this is only needed when you have more than one datasource.
+     *
+     * @return the bean name of {@code SqlSessionFactory}
      */
     String sqlSessionFactoryRef() default "";
 
     /**
      * Specifies a custom MapperFactoryBean to return a mybatis proxy as spring bean.
-     * @return Class.
+     *
+     * @return the class of {@code MapperFactoryBean}
      */
     Class<? extends MapperFactoryBean> factoryBean() default MapperFactoryBean.class;
 
+    /**
+     * Whether enable lazy initialization of mapper bean.
+     * <p>
+     * Default is {@code false}.
+     * </p>
+     *
+     * @return set {@code true} to enable lazy initialization
+     *
+     * @since 2.0.2
+     */
+    String lazyInitialization() default "";
+
+    /**
+     * Specifies the default scope of scanned mappers.
+     * <p>
+     * Default is {@code ""} (equiv to singleton).
+     * </p>
+     *
+     * @return the default scope
+     */
+    String defaultScope() default AbstractBeanDefinition.SCOPE_DEFAULT;
+
+    /**
+     * Specifies a flag that whether execute a property placeholder processing or not.
+     * <p>
+     * The default is {@literal true}. This means that a property placeholder processing execute.
+     *
+     * @since 3.0.3
+     *
+     * @return a flag that whether execute a property placeholder processing or not
+     */
+    boolean processPropertyPlaceHolders() default true;
 }
 
